b418b5bb09fe5a0a1feabbee29b4ff3b1ef62af3
Eric Bradley <131034082+ebradley0@users.noreply.github.com>
2025-04-02 12:03:55 -0400

    Introduced system.h header for organization, began pseudocode of main function

--- msp430fr235x_1.c/msp430fr235x_1.c
+++ msp430fr235x_1.c/msp430fr235x_1.c
@@ -1,13 +1,30 @@
-#include <msp430.h>
+#include "SYSTEM.h"
+#include "msp430fr2355.h"
 
 int main(void)
 {
     //Setup Code (Runs when processor resets)
 
+    WDTCTL = WDTPW | WDTHOLD;
+
+    PM5CTL0 &= ~LOCKLPM5;
+
     while(1)
     {
+        //setIgnitor
+        //turnonPilotValve
+        //Delay 2 seconds
+        //CheckFlame
+        //If flame, release gas
+
+
+        //while(CallforHeat() || checkflame() || (readBoiler < threshhold) ); /Stalls while any of these states are active, this leaves everything open
+
+        //shutDownHeatSys() -- Turns off main valve, pilot valve, and Ignitor
 
         // Loop
 
+        //Idle, any functionality will be done via interrupts.
+        __bis_SR_Register(CPUOFF);
     }
 }
